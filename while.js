10ab57b7bab1bc8acde70d15f40433b081b11c96
Fahima Alo <fahimaalo223@gmail.com>
2024-10-16 13:11:52 +0600

    for while loop completed

---
+++ while.js
@@ -0,0 +1,11 @@
+const number = 7;
+if(number < 1) {
+    console.log('soto akta number paici');
+}
+console.log('something new cooking');
+
+let num = 0;
+while(num <5){
+    console.log("looping", num);
+    num++;
+}
