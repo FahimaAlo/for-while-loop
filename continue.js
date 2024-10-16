a5f54dbee9e353120a1980e210692832c93569bc
Fahima Alo <fahimaalo223@gmail.com>
2024-10-16 20:05:04 +0600

    added looping convension

---
+++ continue.js
@@ -0,0 +1,16 @@
+// for(i = 1; i < 10; i++){
+//     if(i%2===1){
+//         continue;
+//     }
+//     console.log(i);
+// }
+let n = 0;
+while(n < 15){
+    n++;
+    if(n % 5 !== 0){
+        continue;
+    }
+    console.log(n);
+   
+}
+console.log('process exited code');
